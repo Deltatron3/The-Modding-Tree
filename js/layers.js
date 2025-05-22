8be734e9be73f9abfa8c762b44960aebe7ffb622
Deltatron3 <AlphaOmega.AO.321@gmail.com>
2025-05-22 20:02:33 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -6,7 +6,7 @@ addLayer("p", {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "E81414",
+    color: "#e81414",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "Quarks", // Name of prestige currency
     baseResource: "Gluons", // Name of resource prestige is based on
