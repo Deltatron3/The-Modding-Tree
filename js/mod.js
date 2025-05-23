5e5ac6c9db64e93d8676a52d1d26c69910b6e823
Deltatron3 <AlphaOmega.AO.321@gmail.com>
2025-05-23 23:32:44 +0200

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -42,7 +42,7 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
-	if (hasUpgrade('u', 11)) gain = gain.times(2)
+	if (hasUpgrade('u', 11)) gain = gain.base(1)
 	return gain
 }
 
