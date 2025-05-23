64a6361071406ad2b86180980a090451e54ed9be
Deltatron3 <AlphaOmega.AO.321@gmail.com>
2025-05-23 23:00:48 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -14,9 +14,9 @@ addLayer("u", {
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.75, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
-        gain = new Decimal(1)
-        if (hasUpgrade('u', 11)) gain = gain.times(2)
-        return gain
+        let mult = new Decimal(1)
+        if (hasUpgrade('u', 11)) mult = mult.times(2)
+        return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
