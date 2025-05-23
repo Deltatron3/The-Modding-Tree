dc91258c21ff077d5ecd58a0bdc42e7c39e30625
Deltatron3 <AlphaOmega.AO.321@gmail.com>
2025-05-23 21:35:45 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,14 +1,14 @@
-addLayer("p", {
-    name: "Quarks", // This is optional, only used in a few places, If absent it just uses the layer id.
+addLayer("u", {
+    name: "Up Quarks", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "Q", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    position: -1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#e81414",
+    color: "#0914e3",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "Quarks", // Name of prestige currency
+    resource: "Up Quarks", // Name of prestige currency
     baseResource: "Gluons", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
@@ -22,14 +22,99 @@ addLayer("p", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "q", description: "Q: Reset for Quarks", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "u", description: "u: Reset for Up Quarks", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     upgrades: {
         11: {
-        title: "Make",
+        title: "",
         description: "Double your point gain.",
-        cost: new Decimal(1),
+        cost: new Decimal(2),
         },
+    
     },
     layerShown(){return true}
+})
+addLayer("c", {
+    name: "Charm Quarks", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "Q", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#e81414",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "Charm Quarks", // Name of prestige currency
+    baseResource: "Gluons", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.75, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "c", description: "c: Reset for Charm Quarks", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+        layerShown(){return true}
+})
+addLayer("t", {
+    name: "Top Quarks", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "Q", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#19e012",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "Top Quarks", // Name of prestige currency
+    baseResource: "Gluons", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.75, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "t", description: "t: Reset for Top Quarks", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+        layerShown(){return true}
+})
+addLayer("p", {
+    name: "Subatomical Formation", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#9e9e9e",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "Particles", // Name of prestige currency
+    baseResource: "Gluons", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.75, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "p", description: "p: Reset for Particles", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+        layerShown(){return true}
 })
