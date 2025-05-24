9e92a7dc67bdc54f00c6eebbb540b747514a777f
Deltatron3 <AlphaOmega.AO.321@gmail.com>
2025-05-24 22:10:58 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -34,7 +34,7 @@ addLayer("up", {
             title: "Up acceleration",
             description: "Up Quarks boosts gluon gain",
             cost: new Decimal(15),       
-            effect() {
+            effectDisplay () {
             return player.up.points.add(1).pow(0.15)
             },
         },
