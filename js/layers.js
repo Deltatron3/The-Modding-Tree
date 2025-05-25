3fb053772bc283b2cf5f5f9ee8de3d99f47fa3d9
Deltatron3 <AlphaOmega.AO.321@gmail.com>
2025-05-25 21:59:54 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -201,8 +201,11 @@ addLayer("p", {
             requirementDescription: "1 Total particle",
             effectDescription: "Charm quarks boosts gluons slightly",
             done() { return player.p.points.gte(1)}       
-        },
-        
+        }, 
+            effectDisplay() { return "×" + format(this.effect()) },       
+            effect() {
+            return player.charm.points.add(1).pow(0.05)
+        },      
     },
     layerShown(){return true}
 })
