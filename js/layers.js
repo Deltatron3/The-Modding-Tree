d499c7305fc25b799111685b4fb5e4f86ffe0f43
Deltatron3 <AlphaOmega.AO.321@gmail.com>
2025-05-25 21:36:07 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -183,6 +183,7 @@ addLayer("p", {
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.1, // Prestige currency exponent
+    branches: ["up","charm","top"],
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
